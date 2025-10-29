af3ab92bd0a4b84be36eed2ca7a52c91424480e9
MolinaJohnny <johnny.molina.rea@gmail.com>
2025-10-29 20:44:25 -0300

    Agregador cuerpo

--- servidor/src/app.module.ts
+++ servidor/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PublicacionesModule } from './publicaciones/publicaciones.module';
+import { AuthModule } from './auth/auth.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { ComentariosController } from './publicaciones/comentarios.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [PublicacionesModule, AuthModule, UsuariosModule],
+  controllers: [AppController, ComentariosController],
   providers: [AppService],
 })
 export class AppModule {}

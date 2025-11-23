1c459e1b68106f2f35779dc0791e5ae8395ab57f
MolinaJohnny <johnny.molina.rea@gmail.com>
2025-11-23 17:35:12 -0300

    A

--- cliente/src/environments/environment.ts
+++ cliente/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // apiUrl: 'https://johnnymolina-tp2-prog4-2025-c2.onrender.com',
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://johnnymolina-tp2-prog4-2025-c2.onrender.com',
+    // apiUrl: 'http://localhost:3000',
 
 };
